f73ff7cccbf29d8bd15990f8028212c6b3307eb6
Denys Filichkin <denys.philichkin@gmail.com>
2021-08-03 16:06:48 +0300

    Update color-switcher.js

--- src/color-switcher.js
+++ src/color-switcher.js
@@ -5,38 +5,46 @@ const refs = {
   startBtn: document.querySelector('[data-action="start"]'),
   stopBtn: document.querySelector('[data-action="stop"]'),
 };
+// кнопка STOP не активна в начале
+refs.stopBtn.disabled = true;
 
 const randomIntegerFromInterval = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1) + min);
 };
 
 let switchColorsId = null;
+
 // тут включить преремену фона с помощью этого
 function onStartBtnClick() {
   refs.startBtn.disabled = true;
   refs.stopBtn.disabled = false;
+  // добавлю класс для тренировки
+  refs.startBtn.classList.add('disabled');
   onSwitchColorBackground();
   console.log('START');
 }
 
 // выключить когда надоест
-refs.stopBtn.disabled = true;
+
 function onStopBtnClick() {
   refs.stopBtn.disabled = true;
   refs.startBtn.disabled = false;
+  // а здесь удалю класс который добавил в кнопке старт
+  refs.startBtn.classList.remove('disabled');
+
   clearInterval(switchColorsId);
   console.log('STOP');
 }
-
-// слушатели на кнопки СТАРТ и СТОП
-refs.startBtn.addEventListener('click', onStartBtnClick);
-refs.stopBtn.addEventListener('click', onStopBtnClick);
 
 // работаю надэтой функцией
 
 function onSwitchColorBackground() {
   switchColorsId = setInterval(() => {
-    console.log('Вызов покраски фона');
+    // console.log('Вызов покраски фона');
     refs.body.style.backgroundColor = colors[randomIntegerFromInterval(0, colors.length - 1)];
   }, 1000);
 }
+
+// слушатели на кнопки СТАРТ и СТОП
+refs.startBtn.addEventListener('click', onStartBtnClick);
+refs.stopBtn.addEventListener('click', onStopBtnClick);
